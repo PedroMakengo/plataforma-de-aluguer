7c2356df214fafa25728eb62874fd9655c158f7c
PedroMakengo <pedromakengomiguel@gmail.com>
2024-02-21 19:37:23 +0100

    Criando as rotas no swagger

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,17 @@
 import { PrismaClient } from '@prisma/client'
 import express from 'express'
 import router from './routes'
+import swaggerUi from 'swagger-ui-express'
+import * as swaggerDocument from './swagger.json'
 
 const app = express()
-app.use(express.json())
-app.use('/api', router)
-
 export const prisma = new PrismaClient({
   log: ['query'],
 })
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+
+app.use('/api', router)
+app.use(express.json())
 
 app.listen(3000, () => console.log('Server running 3000'))
